c2713f9cd825f96f3e82cbb902b97bd7ced1830b
HenriTeinturier <henri.teinturier@gmail.com>
2025-04-02 16:33:02 +0200

    4.7 artifact fix avec deploy to Github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<nom_du_repo>/",
+  base: "/cicd-formation-lyon-31-03/",
 });
